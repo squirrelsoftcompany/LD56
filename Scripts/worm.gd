9d614f76e17c5eca9c4244372b816ebdce191f7e
AngryCyboot <cyboot25@gmail.com>
2024-10-05 19:51:28 +0200

    fixes and worm butt for camera attraction

--- Scripts/worm.gd
+++ Scripts/worm.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 var worm : Path2D
 var gallery : Path2D
-
-@export var width : float =50 :
+var width : float :
 	set(value):
-		width = value
 		worm.width = value
+		gallery.width = value
+		width = value
+
+@export var start_width : float =50 
 @export var length : float = 300
 
 func _ready():
@@ -15,7 +17,9 @@ func _ready():
 	worm._curve = gallery.curve.duplicate() # do not remove the underscore !!
 	worm.cap_end_cap = 2
 	worm.cap_begin_cap = 2
+	width = start_width
 	worm.width = width
+	gallery.width = width
 
 func _process(_delta):
 	worm._curve = gallery.curve.duplicate()
@@ -23,6 +27,8 @@ func _process(_delta):
 		var butt_progress : float = gallery.curve.get_baked_length()-length
 		while worm.curve.get_baked_length() > length:
 			worm.curve.remove_point(0)
-		worm.curve.add_point(gallery.curve.sample_baked(butt_progress),Vector2.ZERO,Vector2.ZERO,0)
+		var butt_pos : Vector2 = gallery.curve.sample_baked(butt_progress)
+		$WormPath/Butt.position = butt_pos
+		worm.curve.add_point(butt_pos,Vector2.ZERO,Vector2.ZERO,0)
 						#----- move test -----
 	#position += Vector2(1,randf_range(-0.5,1.2))
