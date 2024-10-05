4350921d5403e345cf3bb825b1ed2c8dfc62f128
Xavier Chalut <xavier.chalut@gmail.com>
2024-10-05 23:24:14 +0200

    [FCT] Add food on map

--- Scripts/map_generator.gd
+++ Scripts/map_generator.gd
@@ -1,18 +1,21 @@
-extends Node
+extends Node2D
 
+const FOOD_RAY_LENGTH = 5 # Short ray for stone X food spacing 
 
 @export var stones : Array[Node]
+@export var foods: Array[Node]
 @export var stone_spawn_radius : float
 var rng = RandomNumberGenerator.new()
+var _stone_radius = 20 #Should be transform to a custome variable
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
 
 
 func _random_stone_pos(tile : Tile) -> Vector2:
 	var _valid = false
-	var _stone_radius = 20 #Should be transform to a custome variable
 	var _new_stone_position : Vector2
 	var _new_stone_global_position : Vector2
 	
@@ -29,3 +32,33 @@ func _random_stone_pos(tile : Tile) -> Vector2:
 			or	abs(_stone.global_position.y - _new_stone_global_position.y) < stone_spawn_radius):
 				_valid = false
 	return _new_stone_position
+	
+func _random_food_pos(tile : Tile) -> Vector2:
+	var _valid = false
+	var _new_food_position : Vector2
+	var _new_food_global_position : Vector2
+	
+	while not _valid:
+		var width = tile.get_tile_size().x
+		var height = tile.get_tile_size().y
+		_new_food_position.x = rng.randf_range(-width/2 + _stone_radius, width/2 - _stone_radius)
+		_new_food_position.y = rng.randf_range(-height/2 + _stone_radius, height/2 - _stone_radius)
+		_valid = true
+		# Use the global position to check the promiscuity with stones on the complete map
+		_new_food_global_position = tile.to_global(_new_food_position)
+		for _stone in stones:
+			if (abs(_stone.global_position.x - _new_food_global_position.x) < stone_spawn_radius
+			or	abs(_stone.global_position.y - _new_food_global_position.y) < stone_spawn_radius):
+				#Additional test for stone into radius
+				var space_state = get_world_2d().direct_space_state
+				var origin = _stone.global_position
+				var direction = _stone.global_position - _new_food_global_position
+				var direction_normalized = direction.normalized()
+				var end = _new_food_global_position + direction_normalized * FOOD_RAY_LENGTH
+				var query = PhysicsRayQueryParameters3D.create(origin, end)
+				query.collide_with_areas = true
+				var result = space_state.intersect_ray(query)
+				if (result.collider.is_in_group("Stone")):
+					_valid = false
+
+	return _new_food_position
--- Scripts/tile.gd
+++ Scripts/tile.gd
@@ -1,8 +1,12 @@
 class_name Tile
 extends Node2D
 
-var stone_scene = preload("res://Map/Stone.tscn")
+var stone_scene = preload("res://Scenes/Map/Elements/stone_1.tscn")
+var food_scene = preload("res://Scenes/Map/Elements/food.tscn")
 var _mesh : MeshInstance2D
+
+@export var food_timer_max = 10
+var food_timer_counter = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,17 +15,39 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+func _process(delta: float) -> void:
+	if food_timer_counter > 0:
+		food_timer_counter -= delta
+	else:
+		_generate_food()
+		food_timer_counter = food_timer_max
+	pass
+	
 func get_tile_size()-> Vector2:
 	return _mesh.scale
 
 func _generate_tile_env() -> void:
 	# Generate some stones
 	var _stone_count = MapGenerator.rng.randi_range(5,10)
-	
 	for i in range(_stone_count):
 		var _stone = stone_scene.instantiate()
 		add_child(_stone)
 		_stone.position = MapGenerator._random_stone_pos(self)
 		MapGenerator.stones.push_back(_stone)
+		
+	# Generate some foods
+	var _food_count = MapGenerator.rng.randi_range(80,100)
+	for i in range(_food_count):
+		var _food = food_scene.instantiate()
+		add_child(_food)
+		_food.position = MapGenerator._random_food_pos(self)
+		MapGenerator.foods.push_back(_food)
+	pass
 	
+func _generate_food() -> void:
+	# Generate food
+	var _food = food_scene.instantiate()
+	add_child(_food)
+	_food.position = MapGenerator._random_food_pos(self)
+	MapGenerator.foods.push_back(_food)
 	pass
