1958709bfcb5a0776e68922dae19fed7b1865487
AngryCyboot <cyboot25@gmail.com>
2024-10-06 13:17:23 +0200

    Revert "temp"
    This reverts commit 083dcfd8230b1636a4f14e8cca80e919b5ad094a.

--- Scripts/worm.gd
+++ Scripts/worm.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var worm : Path2D
 var gallery : Path2D
-var worm_sight : Path2D
 var width : float :
 	set(value):
 		worm.width = value
@@ -14,17 +13,12 @@ var width : float :
 
 func _ready():
 	worm = $WormPath
-	worm_sight = $Sight/CamWormSight/WormSight
 	gallery = $Gallery.gallery
 	worm._curve = gallery.curve.duplicate() # do not remove the underscore !!
-	worm_sight._curve = worm.curve
 	worm.cap_end_cap = 2
 	worm.cap_begin_cap = 2
-	worm_sight.cap_end_cap = 2
-	worm_sight.cap_begin_cap = 2
 	width = start_width
 	worm.width = width
-	worm_sight.width = width*4
 	gallery.width = width
 
 func _process(_delta):
@@ -36,6 +30,5 @@ func _process(_delta):
 		var butt_pos : Vector2 = gallery.curve.sample_baked(butt_progress)
 		$WormPath/Butt.position = butt_pos
 		worm.curve.add_point(butt_pos,Vector2.ZERO,Vector2.ZERO,0)
-	worm_sight._curve = worm.curve
 						#----- move test -----
 	#position += Vector2(1,randf_range(-0.5,1.2))
