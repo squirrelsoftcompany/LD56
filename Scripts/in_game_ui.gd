cf7197c5890716eca3d31d21368d3f2b9dc50041
ChristopheClaustre <christophe.claustre.31@gmail.com>
2024-10-07 23:39:59 +0200

    game over screen hidden by default

--- Scripts/in_game_ui.gd
+++ Scripts/in_game_ui.gd
@@ -14,7 +14,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	$GAME_OVER.visible = false
+	$ButtonContainer.visible = false
 
 
 func game_over():
