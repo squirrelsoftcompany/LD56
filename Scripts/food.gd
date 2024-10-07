330662735376fdce8b26b0ef8874a10f23189a4b
Xavier Chalut <xavier.chalut@gmail.com>
2024-10-07 18:52:34 +0200

    [FCT] Add food indicator

---
+++ Scripts/food.gd
@@ -0,0 +1,39 @@
+class_name Food
+extends Sprite2D
+
+@export var indicator : Node2D
+
+@export var max_scale : float
+@export var min_scale : float
+@export var grow_up_distance : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	indicator.visible = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func show_indicator(indicator_global_position : Vector2):
+	indicator.global_position = indicator_global_position
+	indicator.look_at(global_position)
+	indicator.scale.x
+	
+	# Change scale if near
+	var _distance = (indicator_global_position - global_position).length() 
+	if _distance < grow_up_distance:
+		indicator.scale.x = (min_scale + (max_scale-min_scale)*(1-_distance/grow_up_distance))
+		indicator.scale.y = (min_scale + (max_scale-min_scale)*(1-_distance/grow_up_distance))
+	else:
+		indicator.scale.x = min_scale
+		indicator.scale.y = min_scale
+		
+	indicator.visible = true
+	pass
+	
+func hide_indicator():
+	indicator.visible = false
+	pass
--- Scripts/worm_controller.gd
+++ Scripts/worm_controller.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 
 @export var max_rotation_speed_in_degrees := 40 # deg/s
 @export var max_forward_speed := 40 # unit/s
+@export var food_detector : Node2D
 
+var food_indicator_scene = preload("res://Scenes/Map/FoodIndicator.tscn")
+
+var _near_foods : Array[Food]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,6 +26,22 @@ func _process(delta: float) -> void:
 		var forward := _get_forward_from_rotation(rotation)
 		velocity = forward * max_forward_speed
 		move_and_slide()
+	
+	# Remove bad referencies on deleted food
+	var _tmp_near_foods : Array[Food]
+	for _food in _near_foods:
+		if is_instance_valid(_food) :
+			var _direction = _food.global_position - global_position
+			if _direction.length() < (food_detector.get_node("Shape").shape.radius) + 40 and _direction.length() > 70 :
+				_food.show_indicator(global_position + (_direction.normalized() * 30))
+				_tmp_near_foods.push_back(_food) 
+			else:
+				_food.hide_indicator()
+	_near_foods = _tmp_near_foods
+	
+	# Draw food indicator or hide it if to far
+	#for _food in _near_foods:
+		#_food.show_indicator(global_position + (_direction.normalized() * 50))
 
 
 static func _compute_rotation(target_direction : Vector2, initial_rotation: float) -> float:
@@ -34,3 +54,17 @@ static func _compute_rotation(target_direction : Vector2, initial_rotation: floa
 
 static func _get_forward_from_rotation(rot : float) -> Vector2:
 	return Vector2(cos(rot), sin(rot))
+
+
+func _on_food_detector_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Food"):
+		_near_foods.push_back(area.get_parent())
+	pass # Replace with function body.
+
+
+#func _on_food_detector_area_exited(area: Area2D) -> void:
+	#if area.is_in_group("Food"):
+		#var _index = _near_foods.find(area.get_parent())
+		#_near_foods[_index].hide_indicator()
+		#_near_foods.remove_at(_index)
+	#pass # Replace with function body.
