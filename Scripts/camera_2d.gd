822c409e15ceba20f68d74e2b5c3b4b3b3d4a7ff
AngryCyboot <cyboot25@gmail.com>
2024-10-11 20:42:48 +0200

    [experimental] Close-in camera

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,6 +1,10 @@
 extends Camera2D
 
+@export var head_margin : float = 2
 
+@onready var phantom : PhantomCamera2D = $"../PhantomCamera2D"
+@onready var worm : Node2D =$"../WormController/Worm"
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = get_tree().get_first_node_in_group("WormHead").position
@@ -11,20 +15,56 @@ func _process(delta: float) -> void:
 	position = get_tree().get_first_node_in_group("WormHead").position
 	
 	# zoom
-	var bodies := get_tree().get_nodes_in_group("WormBody")
+	var bodies : Array[Node] = get_tree().get_nodes_in_group("WormBody")
 	var worm_pos : Vector2 = bodies[0].global_position
 	var worm_width : float = bodies[0].start_width
-	var distance_needed : float = 0.0
-	for body : Node2D in bodies :
-		DebugDraw2D.rect_filled(body.global_position, Vector2(10,10), Color.WHITE, delta)
-		var current_distance := worm_pos.distance_to(body.global_position)
-		if current_distance > distance_needed:
-			distance_needed = current_distance
-	distance_needed = distance_needed * 2 + worm_width*4 * 2
-	var initial_size := get_viewport_rect().size / zoom
-	var factors := Vector2(distance_needed / initial_size.x, distance_needed / initial_size.y)
-	var zooming_needed = max(factors.x, factors.y)
+	#var distance_needed : float = 0.0
+	#for body : Node2D in bodies :
+		#DebugDraw2D.rect_filled(body.global_position, Vector2(10,10), Color.WHITE, delta)
+		#var current_distance := worm_pos.distance_to(body.global_position)
+		#if current_distance > distance_needed:
+			#distance_needed = current_distance
+	#distance_needed = distance_needed * 2 + worm_width*4 * 2
+	var top : float =bodies[0].global_position.y
+	var bot : float =bodies[0].global_position.y
+	var left : float =bodies[0].global_position.x
+	var right : float =bodies[0].global_position.x
+	phantom.follow_targets.clear()
+	for body in bodies:
+		phantom.append_follow_targets(body)
+		if body.global_position.y < top:
+			top = body.global_position.y
+		elif body.global_position.y > bot:
+			bot = body.global_position.y
+		if body.global_position.x < left:
+			left = body.global_position.x
+		elif body.global_position.x > right:
+			right = body.global_position.x
+	if worm.global_position.y - (worm.length/head_margin) < top:
+		top = worm.global_position.y - (worm.length/head_margin)
+	elif worm.global_position.y + (worm.length/head_margin) > bot:
+		bot = worm.global_position.y + (worm.length/head_margin)
+	if worm.global_position.x - (worm.length/head_margin) < left:
+		left = worm.global_position.x - (worm.length/head_margin)
+	elif worm.global_position.x + (worm.length/head_margin) > right:
+		right = worm.global_position.x + (worm.length/head_margin)
+	var height : float = bot - top + 25
+	var width : float = right - left + 25
+	var needed_ratio : float = width/height
+	var initial_size : Vector2 = get_viewport_rect().size 
+	var initial_ratio : float = initial_size.x /initial_size.y
+	var zooming_needed : float = 1
+	if needed_ratio >= initial_ratio:
+		zooming_needed = initial_size.x/width
+	else :
+		zooming_needed = initial_size.y/height
+	#var initial_size := get_viewport_rect().size / zoom
+	#var factors := Vector2(distance_needed / initial_size.x, distance_needed / initial_size.y)
+	#var zooming_needed = max(factors.x, factors.y)
+	#if not is_equal_approx(zooming_needed, 0):
+		#zoom /= zooming_needed
+		#zoom.x = clampf(zoom.x, 0.25, 4)
+		#zoom.y = clampf(zoom.y, 0.25, 4)
 	if not is_equal_approx(zooming_needed, 0):
-		zoom /= zooming_needed
-		zoom.x = clampf(zoom.x, 0.5, 4)
-		zoom.y = clampf(zoom.y, 0.5, 4)
+		zooming_needed = clampf(zooming_needed, 1, 4)
+		phantom.set_zoom(Vector2(zooming_needed,zooming_needed))
